47b7b3e7d4c4dea6fcd419fb74a68bd0529b3f26
xinyuanliu <window_9@163.com>
2015-04-24 12:43:31 +0800

    var

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-var a;
+var a = function(){
+  var t = new Date();
+  for(var i = 0; i < 100000000; i++){
+    var a = 1;
+    var b = 2;
+    var c = 3;
+  }
+  console.log(new Date() - t);
+}
+
+var b = function(){
+  var t = new Date();
+  for(var i = 0; i < 100000000; i++){
+    var a = 1, b = 2, c = 3;
+  }
+  console.log(new Date() - t);
+}
+
+a();
+b();
